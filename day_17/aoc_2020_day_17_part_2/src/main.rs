5ff905a50ff0f8a98e2d36c3e64cae76aa8262a4
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-10-22 16:34:45 -0400

    Complete day 17 part 2

---
+++ day_17/aoc_2020_day_17_part_2/src/main.rs
@@ -0,0 +1,144 @@
+use std::cmp::{max, min};
+
+fn main() -> anyhow::Result<()> {
+    let input = include_str!("../../test_input.txt");
+    let mut dimension = PocketDimension::from(input);
+
+    for _ in 0..6 {
+        dimension = dimension.next();
+        dimension.display();
+        println!("{}", dimension.count_active_cubes());
+    }
+
+    let result = dimension.count_active_cubes();
+
+    println!("{}", result);
+
+    Ok(())
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Cube {
+    active: bool,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct PocketDimension {
+    // 4D array of 20 by 20 by 13 by 13
+    //
+    // 20 is the maximum possible size for the world since the original
+    // dimensions are 8 x 8 x 1 and the active cube pattern can only grow of 1
+    // unit in each direction (2 units by dimension) by cycle. Over 6 cycles,
+    // this amounts to 12 more units by dimensions. (8 + 12 = 20, 1 + 12 = 13)
+    cubes: [[[[Cube; 13]; 13]; 20]; 20],
+}
+
+impl PocketDimension {
+    fn new() -> PocketDimension {
+        PocketDimension {
+            cubes: [[[[Cube { active: false }; 13]; 13]; 20]; 20],
+        }
+    }
+
+    fn from(input: &str) -> PocketDimension {
+        let input_lines = input.lines().collect::<Vec<_>>();
+        if input_lines.len() != 8 || input_lines[0].len() != 8 {
+            panic!("This input doesn't have the right dimensions.")
+        }
+
+        let mut result = PocketDimension::new();
+
+        for (i, line) in input_lines.iter().enumerate().take(8) {
+            for (j, char) in line.chars().enumerate() {
+                result.cubes[6 + i][6 + j][6][6].active = match char {
+                    '#' => true,
+                    '.' => false,
+                    _ => panic!("Malformed input"),
+                }
+            }
+        }
+
+        result
+    }
+
+    fn count_active_neighbours(&self, x: usize, y: usize, z: usize, w: usize) -> u8 {
+        if x >= 20 || y >= 20 || z >= 13 || w >= 13 {
+            panic!("Can't access this cube");
+        }
+
+        let mut active_neighbours = 0;
+
+        for i in max(0, x as isize - 1) as usize..=min(19, x + 1) {
+            for j in max(0, y as isize - 1) as usize..=min(19, y + 1) {
+                for k in max(0, z as isize - 1) as usize..=min(12, z + 1) {
+                    for l in max(0, w as isize - 1) as usize..=min(12, w + 1) {
+                        if i == x && j == y && k == z && l == w {
+                            continue;
+                        } else if self.cubes[i][j][k][l].active {
+                            active_neighbours += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        active_neighbours
+    }
+
+    fn next(&self) -> PocketDimension {
+        let mut next = PocketDimension::new();
+
+        for x in 0..self.cubes.len() {
+            for y in 0..self.cubes[0].len() {
+                for z in 0..self.cubes[0][0].len() {
+                    for w in 0..self.cubes[0][0][0].len() {
+                        next.cubes[x][y][z][w].active = matches!(
+                            (
+                                self.cubes[x][y][z][w].active,
+                                self.count_active_neighbours(x, y, z, w),
+                            ),
+                            (true, 2) | (_, 3)
+                        )
+                    }
+                }
+            }
+        }
+
+        next
+    }
+
+    fn count_active_cubes(&self) -> u32 {
+        let mut count = 0;
+        for x in 0..self.cubes.len() {
+            for y in 0..self.cubes[0].len() {
+                for z in 0..self.cubes[0][0].len() {
+                    for w in 0..self.cubes[0][0][0].len() {
+                        if self.cubes[x][y][z][w].active {
+                            count += 1;
+                        }
+                    }
+                }
+            }
+        }
+        count
+    }
+
+    fn display(&self) {
+        for w in 0..self.cubes[0][0][0].len() {
+            for z in 0..self.cubes[0][0].len() {
+                println!("\nz = {}, w = {}", z as i32 - 6, w as i32 - 6);
+                for x in 0..self.cubes[0].len() {
+                    println!();
+                    for y in 0..self.cubes.len() {
+                        let symbol = match self.cubes[x][y][z][w].active {
+                            true => '#',
+                            false => '.',
+                        };
+                        print!("{}", symbol);
+                    }
+                }
+                println!();
+            }
+        }
+    }
+}
