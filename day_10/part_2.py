a196c9b5ce5944ec0dd6fe39a181ca6e749b3ad4
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-10-22 13:36:20 -0400

    Complete day 10 part 2

--- day_10/part_2.py
+++ day_10/part_2.py
@@ -1,18 +1,74 @@
-from time import perf_counter
+from time import perf_counter_ns
+from functools import cache
 
-# naive solution
-def naive(joltages):
+# naive solution -> O(N^3)
+def naive(joltages, current_index):
+    current = joltages[current_index]
+    if current == 0:
+        return 1
+    
+    minus_one_index = None
+    minus_two_index = None
+    minus_three_index = None
+    
+    next = current_index - 1
+    
+    if joltages[next] == current - 1:
+        minus_one_index = next
+        next -= 1
+    if joltages[next] == current - 2:
+        minus_two_index = next
+        next -= 1
+    if joltages[next] == current - 3:
+        minus_three_index = next
+    
+    minus_one_result = naive(joltages, minus_one_index) if minus_one_index is not None else 0
+    minus_two_result = naive(joltages, minus_two_index) if minus_two_index is not None else 0
+    minus_three_result = naive(joltages, minus_three_index) if minus_three_index is not None else 0
+
+    return minus_one_result + minus_two_result + minus_three_result
 
 
 def main():
-    f = open("./test_input")
+    f = open("./input")
     joltages = [int(l) for l in f]
     joltages.append(0)
     joltages.sort()
     joltages.append(joltages[-1] + 3)
-    start_t = perf_counter()
-    print(naive(joltages))
-    end_t = perf_counter()
+
+    # still naive but better ;) -> O(N)
+    # include function here to reuse global variable "joltages" and including the array in the dict key
+    @cache
+    def not_as_naive(current_index):
+        current = joltages[current_index]
+        if current == 0:
+            return 1
+
+        minus_one_index = None
+        minus_two_index = None
+        minus_three_index = None
+
+        next = current_index - 1
+
+        if joltages[next] == current - 1:
+            minus_one_index = next
+            next -= 1
+        if joltages[next] == current - 2:
+            minus_two_index = next
+            next -= 1
+        if joltages[next] == current - 3:
+            minus_three_index = next
+
+        minus_one_result = not_as_naive(minus_one_index) if minus_one_index is not None else 0
+        minus_two_result = not_as_naive(minus_two_index) if minus_two_index is not None else 0
+        minus_three_result = not_as_naive(minus_three_index) if minus_three_index is not None else 0
+
+        return minus_one_result + minus_two_result + minus_three_result
+
+    start_t = perf_counter_ns()
+    print(not_as_naive(len(joltages) - 1))
+    # print(naive(joltages, len(joltages) - 1))
+    end_t = perf_counter_ns()
     print(end_t - start_t)
 
 
